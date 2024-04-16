56511b9699266e9a9fc37cd834beb1995a566ce4
Mauricio <stb539@gmail.com>
2024-04-16 13:03:51 -0400

    cuatro_img_un_btn

--- app/src/main/java/com/example/clickactivity/MainActivity.java
+++ app/src/main/java/com/example/clickactivity/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.clickactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Log.d("onCreate", "entr onCreate");
+
+        Button btnSend = findViewById(R.id.imgID);
+
+        // escuchador de un boton
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Snackbar.make(v, "snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                GoSecond(v);
+            }
+        });
+    }
+
+    public void GoSecond(View view) {
+        //EditText edtName = findViewById(R.id.edt_name);
+        //String send_string = edtName.getText().toString();
+        Intent intent = new Intent(this, MainActivity2.class);
+        //intent.putExtra("LINK", send_string);
+        startActivity(intent);
     }
 }
